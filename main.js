a0d422c183ac3612518e3a0db209f6ab53956205
Harshita Mangotra <75719519+harshita-2402@users.noreply.github.com>
2021-10-17 18:15:48 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,52 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+    });
+
+    $('.navbar .menu li a').click(function(){
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+    var typed = new Typed(".typing", {
+        strings: ["Machine Learning Enthusiast, Web Developer, Instrumentalist"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["Machine Learning Enthusiast, Web Developer, Instrumentalist"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+// owl carousel script
+$('.carousel').owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplay: true,
+    autoplayTimeOut: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+        0:{
+            items: 1,
+            nav: false
+        },
+        600:{
+            items: 2,
+            nav: false
+        },
+       
+    }
+});
+    
+});
